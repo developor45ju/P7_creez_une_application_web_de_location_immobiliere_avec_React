eae3040789dd16f222c6113dc54346aac1d2e0fa
Julien Heng <julienheng0@gmail.com>
2024-06-28 11:58:32 +0200

    Created script dor diffrents varibles environments

---
+++ set-homepage.js
@@ -0,0 +1,17 @@
+// Détecte l'environnement de déploiement
+const isGitHubPages = window.location.hostname.endsWith('.github.io');
+const isNetlify = window.location.hostname.endsWith('.netlify.app');
+
+// URLs de la page d'accueil pour chaque environnement
+const homepages = {
+  githubPages: 'https://developor45ju.github.io/kasa-oc-p7/', // Remplacez par vos informations
+  netlify: 'https://agence-kasa.netlify.app/' // Remplacez par votre URL Netlify
+};
+
+// Obtient l'URL correcte en fonction de l'environnement
+const homepageUrl = isGitHubPages ? homepages.githubPages : (isNetlify ? homepages.netlify : null);
+
+// Redirige si nécessaire
+if (homepageUrl && window.location.pathname === '/') {
+  window.location.href = homepageUrl;
+}
